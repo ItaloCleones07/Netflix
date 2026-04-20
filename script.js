1e25e7379446e87c5f76084f39626226361a724e
ItaloCleones07 <italocleones2407@gmail.com>
2026-04-20 10:37:58 -0400

    Verificação de acesso!

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const login = document.querySelector("#login")
 const senha = document.querySelector("#senha")
 const button = document.querySelector("button")
+const mensagem = document.querySelector(".incorreto")
 
 localStorage.setItem('Login', 'italocleones08@gmail.com')
 localStorage.setItem('Senha', '123456')
@@ -9,7 +10,7 @@ function Entrar() {
     if ((login.value === localStorage.getItem('Login')) && (senha.value === localStorage.getItem('Senha'))) {
         window.location.href = './Perfil/perfil.html'
     } else {
-        console.log('Não acessado!')
+        mensagem.innerHTML = 'Login e Senha incorretos!'
     }
 }
 
